f6c304b771229c8bbe9dd8dfef868e7b2bdc327d
Giuliano Losa <giuliano@losa.fr>
2026-03-16 09:56:28 -0700

    shorter proofs

--- OptiRBC/AbstractOptiRBC.thy
+++ OptiRBC/AbstractOptiRBC.thy
@@ -17,19 +17,13 @@ statespace ('p,'v) abs_vars =
   committed :: "'p \<Rightarrow> 'v \<Rightarrow> bool"
 
 locale abs_protocol =
-  abs_vars proposal echo vote ack ready committed
+  abs_vars _ _ _ _ _ _
     project_HOL_bool_'v_fun_'p_fun inject_HOL_bool_'v_fun_'p_fun
     project_HOL_bool_'v_fun inject_HOL_bool_'v_fun +
   abstract_domain_model broadcaster faulty
     opt_quorum_member vote_quorum_member quorum_member
     amplification_quorum_member commit_quorum_member
-  for proposal :: "'a"
-    and echo :: "'a"
-    and vote :: "'a"
-    and ack :: "'a"
-    and ready :: "'a"
-    and committed :: "'a"
-    and project_HOL_bool_'v_fun_'p_fun :: "'b \<Rightarrow> 'p::finite \<Rightarrow> 'v \<Rightarrow> bool"
+  for project_HOL_bool_'v_fun_'p_fun :: "'b \<Rightarrow> 'p::finite \<Rightarrow> 'v \<Rightarrow> bool"
     and inject_HOL_bool_'v_fun_'p_fun :: "('p \<Rightarrow> 'v \<Rightarrow> bool) \<Rightarrow> 'b"
     and project_HOL_bool_'v_fun :: "'b \<Rightarrow> 'v \<Rightarrow> bool"
     and inject_HOL_bool_'v_fun :: "('v \<Rightarrow> bool) \<Rightarrow> 'b"
@@ -132,9 +126,7 @@ definition inductive_invariant where
      (\<forall>c. init c \<longrightarrow> I c)
      \<and> (\<forall>c c'. I c \<longrightarrow> step c c' \<longrightarrow> I c')"
 
-section \<open>Proof methods\<close>
-
-lemma step_cases [consumes 1, case_names byzantine propose echo vote ack ready opt_commit commit]:
+lemma step_cases [consumes 1]:
   assumes "step c c'"
   obtains
     (byzantine) "byzantine_step c c'"
@@ -147,33 +139,11 @@ lemma step_cases [consumes 1, case_names byzantine propose echo vote ack ready o
   | (commit) p v where "commit_step c c' p v"
   using assms unfolding step_def by blast
 
-lemma inductive_invariant_stepsI:
-  assumes init_case: "\<And>c. init c \<Longrightarrow> I c"
-    and byzantine_case: "\<And>c c'. I c \<Longrightarrow> byzantine_step c c' \<Longrightarrow> I c'"
-    and propose_case: "\<And>c c' v. I c \<Longrightarrow> propose_step c c' v \<Longrightarrow> I c'"
-    and echo_case: "\<And>c c' p v. I c \<Longrightarrow> echo_step c c' p v \<Longrightarrow> I c'"
-    and vote_case: "\<And>c c' p v. I c \<Longrightarrow> vote_step c c' p v \<Longrightarrow> I c'"
-    and ack_case: "\<And>c c' p v. I c \<Longrightarrow> ack_step c c' p v \<Longrightarrow> I c'"
-    and ready_case: "\<And>c c' p v. I c \<Longrightarrow> ready_step c c' p v \<Longrightarrow> I c'"
-    and opt_commit_case: "\<And>c c' p v. I c \<Longrightarrow> opt_commit_step c c' p v \<Longrightarrow> I c'"
-    and commit_case: "\<And>c c' p v. I c \<Longrightarrow> commit_step c c' p v \<Longrightarrow> I c'"
-  shows "inductive_invariant I"
-proof (unfold inductive_invariant_def, intro conjI allI impI)
-  fix c
-  assume "init c"
-  then show "I c"
-    by (rule init_case)
-next
-  fix c c'
-  assume inv: "I c" and st: "step c c'"
-  from st show "I c'"
-    by (metis ack_case byzantine_case commit_case echo_case inv opt_commit_case propose_case step_cases
-        ready_case vote_case)
-qed
+lemmas protocol_defs = init_def byzantine_step_def propose_step_def
+echo_step_def vote_step_def ack_step_def ready_step_def opt_commit_step_def commit_step_def
 
-method inductive_invariant_cases = (rule inductive_invariant_stepsI)
-
-section \<open>Safety invariants\<close>
+method try1 =
+   (metis | force | (smt (verit)))
 
 definition inv1 where
   \<comment> \<open>If @{term broadcaster} is faulty and a non-faulty party commits @{term v}, then either
@@ -183,172 +153,29 @@ definition inv1 where
     (\<exists>Q. \<forall>q. amplification_quorum_member Q q \<longrightarrow> q \<notin> faulty \<and> (c\<cdot>ready) q v)
     \<or> (\<exists>Q. \<forall>q. vote_quorum_member Q q \<and> q \<notin> faulty \<longrightarrow> (c\<cdot>echo) q v)"
 
+
 lemma inv1_inductive:
   "inductive_invariant inv1"
-  text \<open>Proof sketch: We use @{thm [source] inductive_invariant_stepsI} to case-split on transitions.
-    The init case is vacuous because no party has committed. Most honest steps do not change
-    @{term committed}, so the antecedent is unchanged; for echo and ready steps the consequent
-    can only become easier to satisfy because they add True entries.
-    For @{term opt_commit_step}: the committing party saw an optimistic quorum of echoes, and
-    @{thm [source] opt_quorum_contains_vote_quorum} extracts a vote quorum of non-faulty
-    members who all echoed the committed value (second disjunct).
-    For @{term commit_step}: the committing party saw a commit quorum of ready parties, and
-    @{thm [source] commit_quorum_contains_amplification_quorum} extracts an amplification
-    quorum of non-faulty members who are all ready (first disjunct).
-    For @{term byzantine_step}: only faulty parties' fields change, and the invariant's
-    consequent refers only to non-faulty parties' @{term ready} and @{term echo}, so it is
-    preserved.\<close>
-proof (inductive_invariant_cases)
-  fix c
-  assume "init c"
-  then show "inv1 c"
-    unfolding inv1_def init_def by simp
-next
-  fix c c'
-  assume inv: "inv1 c" and step: "byzantine_step c c'"
-  then show "inv1 c'"
-    unfolding inv1_def byzantine_step_def
-    apply auto
-    apply meson
-    apply (smt (verit))
-    apply meson
-    apply meson
-    by metis
-next
-  fix c c' v
-  assume inv: "inv1 c" and step: "propose_step c c' v"
-  then show "inv1 c'"
-    unfolding inv1_def propose_step_def by auto
-next
-  fix c c' p v
-  assume inv: "inv1 c" and step: "echo_step c c' p v"
-  then show "inv1 c'"
-    unfolding inv1_def echo_step_def
-    apply auto
-    apply blast
-    by blast
-next
-  fix c c' p v
-  assume inv: "inv1 c" and step: "vote_step c c' p v"
-  then show "inv1 c'"
-    unfolding inv1_def vote_step_def by auto
-next
-  fix c c' p v
-  assume inv: "inv1 c" and step: "ack_step c c' p v"
-  then show "inv1 c'"
-    unfolding inv1_def ack_step_def by auto
-next
-  fix c c' p v
-  assume inv: "inv1 c" and step: "ready_step c c' p v"
-  then show "inv1 c'"
-    unfolding inv1_def ready_step_def
-    apply auto
-    apply blast
-    apply blast
-    apply blast
-    by blast
-next
-  fix c c' p v
-  assume inv: "inv1 c" and step: "opt_commit_step c c' p v"
-  then show "inv1 c'"
-    unfolding inv1_def opt_commit_step_def
-    apply auto
-    by (metis opt_quorum_contains_vote_quorum)
-next
-  fix c c' p v
-  assume inv: "inv1 c" and step: "commit_step c c' p v"
-  then show "inv1 c'"
-    unfolding inv1_def commit_step_def
-    apply auto
-    by (metis commit_quorum_contains_amplification_quorum)
-qed
+  apply (unfold inductive_invariant_def; rule conjI; (unfold step_def)?) \<comment> \<open>First obtain one goal per step case (and init)\<close>
+   apply (simp add:inv1_def init_def) \<comment> \<open>Discharge init\<close>
+  apply (auto elim: step_cases; auto simp add:protocol_defs inv1_def; insert abstract_domain_model_axioms; unfold abstract_domain_model_def) \<comment> \<open>Do cases analysis on the step\<close>
+              apply try1+
+  done
 
 definition inv2 where
   "inv2 c \<equiv> \<forall> p v . broadcaster \<in> faulty \<and> p \<notin> faulty \<and> (c\<cdot>committed) p v \<longrightarrow>
-    (\<exists>Q. \<forall>q. quorum_member Q q \<and> q \<notin> faulty \<longrightarrow> (c\<cdot>ack) q v)
-    \<or> (\<exists>Q. \<forall>q. amplification_quorum_member Q q \<longrightarrow> q \<notin> faulty \<and> (c\<cdot>ready) q v)
+    ((\<exists>Q. \<forall>q. quorum_member Q q \<and> q \<notin> faulty \<longrightarrow> (c\<cdot>ack) q v)
+    \<and> (\<exists>Q. \<forall>q. amplification_quorum_member Q q \<longrightarrow> q \<notin> faulty \<and> (c\<cdot>ready) q v))
     \<or> (\<exists>Q. \<forall>q. vote_quorum_member Q q \<and> q \<notin> faulty \<longrightarrow> (c\<cdot>echo) q v)"
 
 lemma inv2_inductive:
   "inductive_invariant inv2"
-  text \<open>Proof sketch: Case-split on transitions using @{thm [source] inductive_invariant_stepsI}.
-    The init case is vacuous (no party has committed). Most honest steps do not change
-    @{term committed}, so the antecedent is unchanged; echo, ack, and ready steps can only
-    make the consequent easier to satisfy since they add True entries.
-    For @{term opt_commit_step}: the committing party saw an optimistic quorum of echoes, and
-    @{thm [source] opt_quorum_contains_vote_quorum} extracts a vote quorum of non-faulty
-    members who all echoed the committed value (third disjunct).
-    For @{term commit_step}: the committing party saw a commit quorum of ready parties, and
-    @{thm [source] commit_quorum_contains_amplification_quorum} extracts an amplification
-    quorum of non-faulty members who are all ready (second disjunct).
-    For @{term byzantine_step}: only faulty parties' fields change, and the invariant's
-    consequent refers only to non-faulty parties' @{term ack}, @{term ready}, and @{term echo},
-    so it is preserved.\<close>
-proof (inductive_invariant_cases)
-  fix c
-  assume "init c"
-  then show "inv2 c"
-    unfolding inv2_def init_def by simp
-next
-  fix c c'
-  assume inv: "inv2 c" and step: "byzantine_step c c'"
-  then show "inv2 c'"
-    unfolding inv2_def byzantine_step_def
-    apply auto
-    apply meson
-    apply (smt (verit))
-    apply meson
-    apply (smt (verit))
-    by meson
-next
-  fix c c' v
-  assume inv: "inv2 c" and step: "propose_step c c' v"
-  then show "inv2 c'"
-    unfolding inv2_def propose_step_def by auto
-next
-  fix c c' p v
-  assume inv: "inv2 c" and step: "echo_step c c' p v"
-  then show "inv2 c'"
-    unfolding inv2_def echo_step_def
-    apply auto
-    apply blast
-    by blast
-next
-  fix c c' p v
-  assume inv: "inv2 c" and step: "vote_step c c' p v"
-  then show "inv2 c'"
-    unfolding inv2_def vote_step_def by auto
-next
-  fix c c' p v
-  assume inv: "inv2 c" and step: "ack_step c c' p v"
-  then show "inv2 c'"
-    unfolding inv2_def ack_step_def
-    apply auto
-    apply blast
-    apply blast
-    apply (smt (verit))
-    by blast
-next
-  fix c c' p v
-  assume inv: "inv2 c" and step: "ready_step c c' p v"
-  then show "inv2 c'"
-    unfolding inv2_def ready_step_def
-    apply auto
-    apply blast
-    apply blast
-    by blast
-next
-  fix c c' p v
-  assume inv: "inv2 c" and step: "opt_commit_step c c' p v"
-  then show "inv2 c'"
-    unfolding inv2_def opt_commit_step_def
-    apply auto
-    by (metis opt_quorum_contains_vote_quorum)
-next
-  fix c c' p v
-  assume inv: "inv2 c" and step: "commit_step c c' p v"
-  then show "inv2 c'"
-    unfolding inv2_def commit_step_def
-    apply auto
-    by (metis commit_quorum_contains_amplification_quorum)
-qed
+  apply (unfold inductive_invariant_def; rule conjI; (unfold step_def)?) \<comment> \<open>First obtain one goal per step case (and init)\<close>
+   apply (simp add:inv2_def init_def) \<comment> \<open>Discharge init\<close>
+  apply (auto elim: step_cases; auto simp add:protocol_defs inv2_def; insert abstract_domain_model_axioms; unfold abstract_domain_model_def) \<comment> \<open>Do cases analysis on the step\<close>
+              apply try1+
+  done
+
+end
+
+end
